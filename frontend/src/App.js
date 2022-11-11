9db137760ce588cbe33deafb009cd98ea074eb8b
sainingo <nickmentor421@gmail.com>
2022-11-11 19:12:45 +0300

    Create div

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Header from './components/Header';
 import Navbar from './components/Navbar';
+import Tutors from './components/Tutors';
 
 function App() {
   return (
     <div>
       <Header />
+      <Tutors />
      <Navbar />
-    
     </div>
   );
 }
--- frontend/src/components/Tutors.jsx
+++ frontend/src/components/Tutors.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import {TfiUser} from 'react-icons/tfi'
 
 const Tutors = () => {
   return (
-    <div>Tutors</div>
+    <div className='w-[80%] absolute ml-[30%] mt-[20%]'>
+      <h3 className='font-semibold ml-10'>Online Tutors</h3>
+      <div>
+        <div className='border border-gray-300 p-4 m-2 w-[40%] rounded-lg'>
+          <div className='flex justify-between'>
+            <div className='flex gap-2'>
+            <TfiUser />
+            <div>
+              <h5>Paul D'lingo</h5>
+              <p>Corsica, France</p>
+            </div>
+            </div>
+            <span className='bg-green-500 h-[16px] p-2 rounded-full'></span>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
