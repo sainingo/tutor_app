e2fa5725583c21c6eed0125c6659ef51d58c4f13
sainingo <nickmentor421@gmail.com>
2022-11-12 16:23:20 +0300

    Prepare backend for deployment

--- frontend/src/features/tutors/tutorsApi.js
+++
@@ -1,7 +0,0 @@
- const getTutors = async (baseURL) => {
-  const response = await fetch(`${baseURL}/api/v1/tutors`)
-  const result = await response.json()
-  return result
-}
-
-export default getTutors;
